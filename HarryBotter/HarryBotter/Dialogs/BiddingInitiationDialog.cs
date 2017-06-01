d87227d09f5cc1a3b29a050951d91b42d63a319e
Brandon Truong <brandon.truong@pickles.com.au>
2017-06-01 16:42:00 +1000

    added carousel of car images

--- HarryBotter/HarryBotter/Dialogs/BiddingInitiationDialog.cs
+++ HarryBotter/HarryBotter/Dialogs/BiddingInitiationDialog.cs
@@ -4,6 +4,9 @@ using System.Threading.Tasks;
 using HarryBotter.DataService;
 using Microsoft.Bot.Builder.Dialogs;
 
+using Microsoft.Bot.Connector;
+using System.Collections.Generic;
+
 namespace HarryBotter.Dialogs
 {
     [Serializable]
@@ -20,14 +23,60 @@ namespace HarryBotter.Dialogs
 
         public async Task StartAsync(IDialogContext context)
         {
-            var vehicles =
-                new AuctionsDataService().ListVehicles(_auctionName, _make)
+            context.Wait(this.MessageReceivedAsync);
+        }
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var reply = context.MakeMessage();
+
+            reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+            reply.Attachments = GetCardsAttachments();
+
+            await context.PostAsync(reply);
+
+            context.Wait(this.MessageReceivedAsync);
+        }
+
+        private IList<Attachment> GetCardsAttachments()
+        {
+
+            return new AuctionsDataService().ListVehicles(_auctionName, _make)
                     .ToList()
-                    .Select(c => $"{c.Colour} {c.Make} made in {c.Model_Year}: {c.Description}");
+                    .Select(c => GetHeroCard(
+                            string.Format("{0} {1} {2}", c.Model_Year, c.Make, c.Body_Tyep),
+                            string.Format("{0} {1} {2}", c.Auction_Sale_Type, c.Colour, c.Fuel_Type),
+                            c.Description,
+                            new CardImage(url: string.Format("https://www.pickles.com.au/getPublicStockImage?id=652327{0}", c.Id.Substring(0,3))),
+                            new CardAction(ActionTypes.OpenUrl, "Place a bid", value: "https://azure.microsoft.com/en-us/services/storage/")
+                            )).ToList();
+        }
 
-            PromptDialog.Choice(context, HandleVehcile, vehicles,
-                $"We have {vehicles.Count()} vehicles available. Choose one to start bidding");
+        private static Attachment GetHeroCard(string title, string subtitle, string text, CardImage cardImage, CardAction cardAction)
+        {
+            var heroCard = new HeroCard
+            {
+                Title = title,
+                Subtitle = subtitle,
+                Text = text,
+                Images = new List<CardImage>() { cardImage },
+                Buttons = new List<CardAction>() { cardAction },
+            };
 
+            return heroCard.ToAttachment();
+        }
+
+        private static Attachment GetThumbnailCard(string title, string subtitle, string text, CardImage cardImage, CardAction cardAction)
+        {
+            var heroCard = new ThumbnailCard
+            {
+                Title = title,
+                Subtitle = subtitle,
+                Text = text,
+                Images = new List<CardImage>() { cardImage },
+                Buttons = new List<CardAction>() { cardAction },
+            };
+
+            return heroCard.ToAttachment();
         }
 
         private async Task HandleVehcile(IDialogContext context, IAwaitable<string> result)
