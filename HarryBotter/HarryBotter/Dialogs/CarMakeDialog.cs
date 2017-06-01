fc2cd1e8a47db214d9d03210841f6119b7ab65c1
togafninja <hamasha@gmail.com>
2017-06-01 13:38:55 +1000

    Added skeleton for CarMakeDialog

--- HarryBotter/HarryBotter/Dialogs/CarMakeDialog.cs
+++ HarryBotter/HarryBotter/Dialogs/CarMakeDialog.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using HarryBotter.DataService;
 
 namespace HarryBotter.Dialogs
 {
     [Serializable]
     public class CarMakeDialog : IDialog<object>
     {
-        public Task StartAsync(IDialogContext context)
+        public async Task StartAsync(IDialogContext context)
         {
-            throw new NotImplementedException();
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var message = await result;
+            await context.PostAsync("Hey there! You said: " + message.Text);
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task AfterMakeAsync(IDialogContext context, IAwaitable<string> result)
+        {
+            var text = await result;
+
+            await context.PostAsync("inside AfterResetAsync");
+
+            context.Wait(MessageReceivedAsync);
         }
     }
 }
