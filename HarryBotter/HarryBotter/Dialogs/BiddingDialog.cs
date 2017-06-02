1fb1d0f947d9ceaf2d0b18f59aee7be7c948a5ab
togafninja <hamasha@gmail.com>
2017-06-02 15:07:40 +1000

    Bid options

--- HarryBotter/HarryBotter/Dialogs/BiddingDialog.cs
+++ HarryBotter/HarryBotter/Dialogs/BiddingDialog.cs
@@ -11,10 +11,32 @@ namespace HarryBotter.Dialogs
         {
             
         }
-        public Task StartAsync(IDialogContext context)
+        public async Task StartAsync(IDialogContext context)
         {
-            throw new NotImplementedException();
-            //Start the bidding timer!
+            PromptDialog.Choice(context, this.AfterSelectOption, 
+                new string[] { "Bid Now", "Select another car", "Stop Bidding?" },
+                "Current bid is A$ 5000. Raise Bid?");
+        }
+
+        private async Task AfterSelectOption(IDialogContext context, IAwaitable<string> result)
+        {
+            var message = await result;
+            if (message == "Bid Now")
+            {
+                await context.PostAsync("Great, Bid Now selected");
+                context.Done("StillBidding"); //Finish this dialog
+            }
+            else if (message == "Select another car")
+                {
+
+                await context.PostAsync("Great, I'll show you more cars");
+                context.Done("ShowCarousel"); //Finish this dialog
+            }
+            else
+            {
+                await context.PostAsync("Ok sure");
+                context.Done("ShowCarousel"); //Finish this dialog
+            }
         }
     }
 }
